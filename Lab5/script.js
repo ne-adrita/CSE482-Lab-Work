3ad3490d330c07f0c486595f21b670b096ef1f46
neadrita <ne.adrita@gmail.com>
2026-07-22 11:14:34 +0600

    Update Lab5 index1.html and script.js

--- Lab5/script.js
+++ Lab5/script.js
@@ -13,3 +13,17 @@ var divBox = document.getElementById("dom-1");
 divBox.addEventListener("click",()=> {
    console.log("Clicked!!")
 });
+
+var output= document.getElementById("out")
+var input1= document.getElementById("inp-1")
+var input2= document.getElementById("inp-2")
+var outputButton= document.getElementById("btn")
+
+outputButton.addEventListener("click",()=> {
+    var x = parseInt(input1.value);
+    var y = parseInt(input2.value);
+    var sum = x+y;
+    console.log(sum);
+    output.innerText=sum;
+    output.innerHTML= `<div style="color: red"><p> ${sum} </p></div>`   // ` refers dynamic string
+})
