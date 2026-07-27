903792a306b8c523a223032e566c9337af94f3ca
neadrita <ne.adrita@gmail.com>
2026-07-28 04:35:18 +0600

    Update Lab6 index.html, style.css and add script.js

---
+++ Lab6/script.js
@@ -0,0 +1,7 @@
+var pg1 = document.getElementById("pg1");
+var btn1 = document.getElementById("btn-1");
+var linkTitle="Facebook";
+
+btn1.addEventListener("click", () => {
+pg1.classList.add("text-uppercase");
+});
